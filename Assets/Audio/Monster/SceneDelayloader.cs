2148e47d7fdcbaff32f9a44839501f51df20f844
Colby Crutcher <94940284+colbycrutcher@users.noreply.github.com>
2025-06-01 23:02:47 -0700

    Death Scene

---
+++ Assets/Audio/Monster/SceneDelayloader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections; // ← this line fixes your error!
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDelayLoader : MonoBehaviour
+{
+    public string sceneToLoad = "NextScene"; // set in Inspector
+    public float delaySeconds = 5f;
+
+    void Start()
+    {
+        StartCoroutine(LoadSceneAfterDelay());
+    }
+
+    IEnumerator LoadSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
